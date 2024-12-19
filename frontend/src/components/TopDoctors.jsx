1eab1ac979748264c02e1f9d6776affba808ff1d
PORAMATRUNGJIRATHANAKUL <poramat.rung@bumail.net>
2024-12-19 15:13:54 +0700

    Banner

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-import { doctors } from "../assets/assets";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
+  const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
+
   return (
     <div className="flex flex-col items-center gap-4 py-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium ">Top Doctors to Book</h1>
@@ -12,6 +16,7 @@ const TopDoctors = () => {
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm-px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
+            onClick={() => navigate(`/appointment/${item._id}`)}
             className="border border-blue-200 rounder-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
           >
@@ -27,7 +32,15 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button>More</button>
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
+        More
+      </button>
     </div>
   );
 };
