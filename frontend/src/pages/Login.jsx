e104fc9170e5459657208c2431e13796c3ebea41
PORAMATRUNGJIRATHANAKUL <poramat.rung@bumail.net>
2024-12-20 16:54:08 +0700

    Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,15 +20,17 @@ const Login = () => {
           Please {state === "Sign Up" ? "sign up" : "log in"} to book
           appointment
         </p>
-        <div className="w-full">
-          <p>Full Name</p>
-          <input
-            className="border border-zinc-300 rounded w-full p-2 mt-1"
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-        </div>
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              className="border border-zinc-300 rounded w-full p-2 mt-1"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </div>
+        )}
         <div className="w-full">
           <p>Email</p>
           <input
