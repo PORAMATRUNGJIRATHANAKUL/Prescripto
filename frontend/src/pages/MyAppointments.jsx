4b4aacdc009f2c7633b3337adb93f595046d7d38
PORAMATRUNGJIRATHANAKUL <poramat.rung@bumail.net>
2024-12-23 10:43:05 +0700

    MyAppointment

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const MyAppointments = () => {
-  return <div></div>;
+  const { doctors } = useContext(AppContext);
+  return (
+    <div>
+      <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">
+        My Appointments
+      </p>
+      <div className="flex flex-col gap-4">
+        {doctors.slice(0, 4).map((item, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-[3fr_18fr_3fr] gap-2 py-4 border-b"
+          >
+            <div className="w-64 bg-indigo-50">
+              <img
+                src={item.image}
+                alt=""
+                className="w-full h-48 object-cover rounded-lg"
+              />
+            </div>
+            <div className="flex-1 text-sm text-zinc-600">
+              <p className="text-neutral-800 font-semibold">{item.name}</p>
+              <p>{item.speciality}</p>
+              <div className="">
+                <p className="text-zinc-700 font-medium mt-1">Address</p>
+                <p className="text-xs">{item.address.line1}</p>
+                <p className="text-xs">{item.address.line2}</p>
+              </div>
+              <p>
+                <span className="text-sm  text-neutral-700 font-medium">
+                  Date & Time:
+                </span>{" "}
+                <span className="text-xs mt-1">25, July, 2024 10:00 AM</span>
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 justify-end">
+              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300">
+                Pay Online
+              </button>
+              <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300">
+                Cancel appointment
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
